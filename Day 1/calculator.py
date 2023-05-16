8216997a50e3ac311fa53d912314d69e22d03ea7
+ <+>
2023-05-16 09:21:29 +0300

    calculator

--- Day 1/calculator.py
+++ Day 1/calculator.py
@@ -1,25 +1,28 @@
 def add(a, b):
-    return a+b
+    return a + b
+
 def sub(a, b):
-    return a-b
+    return a - b
+
 def mul(a, b):
-    return a-b
+    return a * b
+
 def div(a, b):
-    return a/b
+    return a / b
 
 print("Enter Two Numbers: ", end="")
 nOne = int(input())
 nTwo = int(input())
-print("Enter the Operator (+,-,*,/): ", end="")
+print("Enter the Operator (+, -, *, /): ", end="")
 ch = input()
-if ch=='+':
-    print("\n" +str(nOne)+ " + " +str(nTwo)+ " = " +str(add(nOne, nTwo)))
-elif ch=='-':
-    print("\n" +str(nOne)+ " - " +str(nTwo)+ " = " +str(sub(nOne, nTwo)))
-elif ch=='*':
-    print("\n" +str(nOne)+ " * " +str(nTwo)+ " = " +str(mul(nOne, nTwo)))
-elif ch=='/':
-    print("\n" +str(nOne)+ " / " +str(nTwo)+ " = " +str(div(nOne, nTwo)))
+
+if ch == '+':
+    print("\n" + str(nOne) + " + " + str(nTwo) + " = " + str(add(nOne, nTwo)))
+elif ch == '-':
+    print("\n" + str(nOne) + " - " + str(nTwo) + " = " + str(sub(nOne, nTwo)))
+elif ch == '*':
+    print("\n" + str(nOne) + " * " + str(nTwo) + " = " + str(mul(nOne, nTwo)))
+elif ch == '/':
+    print("\n" + str(nOne) + " / " + str(nTwo) + " = " + str(div(nOne, nTwo)))
 else:
-    
     print("\nInvalid Operator!")
