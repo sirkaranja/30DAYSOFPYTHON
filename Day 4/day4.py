59f91bd01a2b2c701d591b82204d4d76d2c7b9a9
+ <+>
2023-05-20 22:43:17 +0300

    day 4 done

---
+++ Day 4/day4.py
@@ -0,0 +1,12 @@
+
+
+def addition():
+    num = int(input("enter num 1:"))
+    num2 = int(input("enter num 2 :"))
+    num3 = int(input("enter num 3:"))
+    num4 = int(input("enter num 4:"))
+    arry_sum= num+num2+num3+num4
+    return(arry_sum)
+
+result = addition()
+print("the sum is", result)
