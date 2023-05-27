a554ec5e7d11ecb86f1b663df4834aae0d552a3b
+ <+>
2023-05-27 15:09:05 +0300

    30daysofpython

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -3,3 +3,10 @@ my_tuple=(1,2,3,4,5,90)
 print( "r" in my_tuple) #this returns false since we dont have r in my_tuple
 
 #difference between list and tuples is that fro tuples you cant change the values once declared(IMMUTABLE VALUES)
+
+#list to tuples
+
+num_list =[1,2,3,4,6,7]
+
+#tuple key word is used in converting list to tuples
+print(tuple(num_list))  
