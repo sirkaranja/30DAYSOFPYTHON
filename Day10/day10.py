801b79f246f9d282116d7cd6ccf3c64b2fd11c0a
+ <sirkaranja12@gmail.com>
2023-06-13 13:54:34 +0300

    updated question2

--- Day10/day10.py
+++ Day10/day10.py
@@ -4,3 +4,18 @@ word = input("Enter a word: ")
 
 word_without_vowels = ''.join([char for char in word if char.lower() not in vowels])
 print(word_without_vowels)
+
+
+#second question checking if all letter start with P
+
+word1= "Peter"
+word2= "Photography"
+
+def check_for_p(word1, word2):
+    if list(word1)[0] == list(word2)[0]:
+        return True
+    else:
+        return False
+
+#calling back the function and passing urguments
+print(check_for_p(word1, word2))
