c79c3c6cb706032e43b2244769812b56737c0285
Daniel Karanja <sirkaranja12@gmail.com>
2023-06-15 14:27:36 -0700

    day 11

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,20 +1,28 @@
 class Employee:
-    def __init__(self,  emp_name, emp_id, emp_salary, emp_department):
-        self.emp_name= emp_name
+    def __init__(self, emp_name, emp_id, emp_salary, emp_department):
+        self.emp_name = emp_name
         self.emp_id = emp_id
-        self.emp_salary= emp_salary
-        self.emp_department= emp_department
+        self.emp_salary = emp_salary
+        self.emp_department = emp_department
 
-    def calculate_emp_salary():
-            pass
+    def calculate_emp_salary(self):
+        salary = float(input("Enter salary: "))
+        hours_worked = float(input("Enter hours worked: "))
+
+        if hours_worked > 50:
+            overtime = hours_worked - 50
+            overtime_amount = overtime * (salary / 50)
+            total_salary = overtime_amount + salary
+            return total_salary
+        else:
+            return 0
 
     def emp_assign_department(self, department):
         self.emp_department = department
-           
-
 
     def print_employee_details(self):
-             return f"{self.emp_name}{self.emp_id} {self.emp_salary}{self.emp_department}"
+        return f"{self.emp_name} {self.emp_id} {self.emp_salary} {self.emp_department}"
+
 
 emp1 = Employee("ADAMS", "E7876", 50000, "ACCOUNTING")
 
@@ -23,4 +31,6 @@ print(emp1.print_employee_details())
 emp1.emp_assign_department("HR")
 
 print(emp1.print_employee_details())
-        
+
+total_salary = emp1.calculate_emp_salary()
+print(f"Total Salary: {total_salary}")
